ab8fcbdda8f57fbb8b5aa98911983d70bc2fe070
燎 石橋 <150461@jc-21.jp>
2017-12-04 18:10:37 +0900

    プロジェクト ファイルを追加します。

---
+++ TabbedPage with NavigationPage/TabbedPage_with_NavigationPage/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabbedPage_with_NavigationPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
