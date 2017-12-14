be2df790fc8519f15a04530362c53de53a6ecbf2
柊 佐藤 <150036@jc-21.jp>
2017-12-14 11:01:28 +0900

    修正10

--- TabbedPage with NavigationPage/TabbedPage_with_NavigationPage/MainPageCS.cs
+++ TabbedPage with NavigationPage/TabbedPage_with_NavigationPage/MainPageCS.cs
@@ -16,6 +16,11 @@ namespace TabbedPage_with_NavigationPage //content C#で作った
             navigationPage2.Icon = "ic_home.png";
             navigationPage2.Title = "記録";
 
+            private void Button_Clicked(object sender, EventArgs e)
+            {
+
+            }
+
 
             var navigationPage = new NavigationPage(new GraphPageCS ());
             navigationPage.Icon = "ic_home.png";
